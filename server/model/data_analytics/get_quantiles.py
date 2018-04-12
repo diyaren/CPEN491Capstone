6189c8ab17ce39c99e8d319d30f839c9fa1815d0
William Phan <wphan@live.ca>
2018-04-12 04:09:17 -0700

    update data analytics stuff and add more metrics to random forest training

--- server/model/data_analytics/get_quantiles.py
+++ server/model/data_analytics/get_quantiles.py
@@ -1,8 +1,11 @@
 import os
 import numpy as np
+import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 
+
 features = np.load("../research/myfeatures.npy")
+
 feature_labels = [
     "Speed Quantiles 1",    # 0
     "Speed Quantiles 2",
@@ -74,61 +77,49 @@ feature_labels = [
     "Head Quantiles Y 8",
     "Head Quantiles Y 9",
     "Head Quantiles Y 10",
-    "Total Energy",                 # 70
-    "Energy Over Distance",         # 71
-    "Enery Over Time",              # 72
-    "Speed Standard Deviation",     # 73
-    "Average Speed (m/s)",          # 74
-    "Speed Variance",               # 75
-    "Braking Strength Standard Deviation",          # 76
-    "Braking Strength Average (m/s^2)",             # 77
-    "Braking Strength Variance",                    # 78
-    "Acceleration Standard Deviation",              # 79
-    "Average Acceleration (m/s^2)",                 # 80
-    "Acceleration Variance",                        # 81
-    "Acceleration From Stop Standard Deviation",    # 82
-    "Average Acceleration From Stop",               # 83
-    "Acceleration From Stop Max"                    # 84
 ]
+
 
 def get_drivers_feature(driver_idx, trip_idx, feature_idx):
     return features[200 * driver_idx + trip_idx][feature_idx]
 
-def plot_features(driver_idxs, feature_idxs, drivers_color):
+def plot_features(axis_title, driver_idxs, feature_idxs, driver_colors):
     """
-    :param driver_idxs: list of driver_idxs to plot for
-    :param feature_idx: list of feature_idxs to plot, this must be 2
     """
-    feature0 = []
-    feature1 = []
-    for driver in driver_idxs:
-        driver_feature0 = []
-        driver_feature1 = []
-        for trip in list(range(200)):  # trips per driver
-            driver_feature0.append(get_drivers_feature(driver, trip, feature_idxs[0]))
-            driver_feature1.append(get_drivers_feature(driver, trip, feature_idxs[1]))
-        feature0.extend(driver_feature0)
-        feature1.extend(driver_feature1)
-
-    colors = []
-    # each color different driver
-    for idx, i in enumerate(driver_idxs):
-        # each driver has 200 samples
+    all_y = []
+    all_x = []
+    for driver_idx in driver_idxs:
+        driver_y = []
+        driver_x = []
         for i in list(range(200)):
-            colors.append(drivers_color[idx])
+            y = []
+            for feature in feature_idxs:
+                trip = i
+                y.append(get_drivers_feature(driver_idx, trip, feature))
+            driver_y.append(y)
+            driver_x.append(np.linspace(0.02, 0.99, len(feature_idxs)))
+        all_y.append(driver_y)
+        all_x.append(driver_x)
 
-    plt.scatter(feature0, feature1, c=colors)
-    plt.xlabel(feature_labels[feature_idxs[0]], fontsize=26)
-    plt.ylabel(feature_labels[feature_idxs[1]], fontsize=26)
-    plt.title("{0} vs {1}".format(feature_labels[feature_idxs[0]], feature_labels[feature_idxs[1]]), fontsize=26)
+    plt.scatter(all_x, all_y, c=driver_colors)
+    plt.xlabel("Percentiles", fontsize=26)
+    plt.ylabel(axis_title, fontsize=26)
+    plt.title("Drivers {}".format(str(driver_idxs)), fontsize=26)
     plt.tick_params(axis="both", labelsize=20)
     plt.show()
 
 if __name__ == "__main__":
-    #print(get_drivers_feature(0, i, 83))
-    #plot_features([21,34], [74, 77], ['C4', 'C2'])
-    plot_features([32,79], [83, 80], ["C4", "C2"])
-    plot_features([32,77], [83, 80], ["C4", "C2"])
-    plot_features([32,53], [83, 80], ["C4", "C2"])
-    plot_features([32,12], [83, 80], ["C4", "C2"])
-    #plot_features([21,34, 5], [70, 80], ["green", "purple", "blue"])
+    #plot_features("Speed Quantiles", 32, list(range(25)), "C1")
+    #plot_features("Speed Quantiles", 33, list(range(25)), "C2")
+    #plot_features("Heading Y Quantiles", [12, 47], list(range(50, 60)), ["C3", "C4"])
+
+    plot_features("Speed Quantiles", [34], list(range(25)), ["C1"])
+    plot_features("Speed Quantiles", [12], list(range(25)), ["C2"])
+    plot_features("Speed Quantiles", [22], list(range(25)), ["C3"])
+    plot_features("Speed Quantiles", [82], list(range(25)), ["C4"])
+    plot_features("Speed Quantiles", [11], list(range(25)), ["C5"])
+    #plot_features("Speed Quantiles", [38, 39], list(range(25)), ["C1", "C2"])
+    #plot_features("Speed Quantiles", [40, 31], list(range(25)), ["C2", "C6"])
+    #plot_features("Acceleration Quantiles", 33, list(range(25, 49)), "C4")
+    #plot_features("Heading Quantiles X", 32, list(range(50, 60)), "C5")
+    #plot_features("Heading Quantiles X", 33, list(range(50, 60)), "C6")
